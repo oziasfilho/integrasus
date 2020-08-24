460999620dd843caf6d50d075b5d7697ddc34603
oziasfilho <48557206+oziasfilho@users.noreply.github.com>
2020-08-24 13:40:50 -0300

    Update integrasus.R

--- integrasus.R
+++ integrasus.R
@@ -4,17 +4,16 @@ library(jsonlite)
 library(tidyverse)
 library(lubridate)
 
-
-link_sabrina<-"https://indicadores.integrasus.saude.ce.gov.br/api/analise-flexibilizacao/leitos-ocupados-uti?idMunicipio=&idMacrorregiao=&idRegiaoSaude="
-teste<-GET(link_sabrina)
-teste$
-  content(teste,"raw")
-dados<-fromJSON(content(teste, "text"))
+#### Link de Leitos Ocupados - UTI
+link_leitos_ocupados_uti<-"https://indicadores.integrasus.saude.ce.gov.br/api/analise-flexibilizacao/leitos-ocupados-uti?idMunicipio=&idMacrorregiao=&idRegiaoSaude="
+link_dados<-GET(link_leitos_ocupados_uti)
+#### Organizando os dados
+dados<-fromJSON(content(link_dados, "text"))
 glimpse(dados)
-
 dados$data<-as.Date(dados$data)
+##### Média de Leitos Ocupados por mês
 media_mes<-dados %>% group_by(month(data,label = TRUE,abbr = FALSE)) %>% summarise(media_leitos=mean(leitosOcupados))
 media_mes$media_leitos<-round(media_mes$media_leitos,2)
-mean(dados$leitosOcupados)
 
-write.table(dados, file = "Dados_Sabrina.csv",row.names=FALSE, na="", sep=";")
+##### Média de Leitos Ocupados Geral
+mean(dados$leitosOcupados)
